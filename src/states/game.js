71bc246ed3fbbf0431020e1ad413af9df35c31c3
Tom Craven <tom.cass.craven@gmail.com>
2016-02-10 22:02:20 +0000

    states/game: revert back to arcade physics & major clean up

--- src/states/game.js
+++ src/states/game.js
@@ -26,14 +26,10 @@ Game.prototype =
         blackHole_mass: 500000,
         camera_cutoff_x: 500,
 
-		physics_type: Phaser.Physics.P2JS,
-
 		render_body_debug_info: true
     },
 
     __origin : new Phaser.Point(0, 0),
-
-	_ship_previous_position : null,
 
     onload : function()
     {
@@ -48,7 +44,7 @@ Game.prototype =
 
     create : function()
     {
-        this.game.physics.startSystem(this.__parameters.physics_type);
+        this.game.physics.startSystem(Phaser.Physics.Arcade);
 
 		this.__sprites.ship = this.__createShip();
 		this.__sprites.blackHole = this.__createBlackHole();
@@ -69,13 +65,11 @@ Game.prototype =
 		var ship = this.game.add.sprite(
 			100, 200, this.__assets.ship.name);
 		ship.anchor.set(0.5, 0.5);
-		this.game.physics.p2.enable(ship);
+		this.game.physics.arcade.enable(ship);
 		ship.body.mass = this.__parameters.ship_mass;
 		ship.checkWorldBounds = true;
 		ship.events.onOutOfBounds.add(this.__shipOutOfBounds, true);
 		this.__ship_previous_position = ship.position;
-
-		window.ship = ship;
 
 		return ship;
 	},
@@ -85,17 +79,10 @@ Game.prototype =
 		var blackHole = this.game.add.sprite(
 			200, 300, this.__assets.blackHole.name);
 		blackHole.anchor.set(0.5, 0.5);
-		this.game.physics.p2.enable(blackHole);
+		this.game.physics.arcade.enable(blackHole);
 		blackHole.body.mass = this.__parameters.blackHole_mass;
 
-		window.blackHole = blackHole;
-
 		return blackHole;
-	},
-
-	__shipOutOfBounds : function()
-	{
-		//Core.startState( STATE_NAME.GAME_OVER );
 	},
 
     update : function()
@@ -104,45 +91,36 @@ Game.prototype =
         var blackHole = this.__sprites.blackHole;
 
         var totalGravity = (ship.body.mass * blackHole.body.mass) /
-            Phaser.Math.distanceSq(ship.body.x, ship.body.y, blackHole.x, blackHole.y);
+            Phaser.Math.distanceSq(ship.x, ship.y, blackHole.x, blackHole.y);
 
         var angleBetweenShipBH = Phaser.Math.angleBetweenPoints(ship.position,
 																blackHole.position);
-		ship.body.force.x = Math.cos(angleBetweenShipBH) * totalGravity;
-		ship.body.force.y = Math.sin(angleBetweenShipBH) * totalGravity;
+		ship.body.gravity.setTo(Math.cos(angleBetweenShipBH) * totalGravity,
+								Math.sin(angleBetweenShipBH) * totalGravity);
 
-		ship.body.thrust(100);
-
-        ship.body.angle = 270 + Phaser.Math.radToDeg(
+        ship.body.rotation = 270 + Phaser.Math.radToDeg(
             Phaser.Math.angleBetweenPoints(ship.body.velocity, this.__origin));
 
         if (this.input.activePointer.leftButton.isDown) {
-            blackHole.body.x = this.camera.x + this.input.mousePointer.position.x;
-            blackHole.body.y = this.camera.y + this.input.mousePointer.position.y;
+            blackHole.position.setTo(this.camera.x + this.input.mousePointer.position.x,
+									 this.camera.y + this.input.mousePointer.position.y);
         }
 
-		var shipPositionPastCutoff = ship.position.x > this.__parameters.camera_cutoff_x;
+		var shipPositionPastCutoff = ship.body.position.x > this.__parameters.camera_cutoff_x;
 		var shipMovingForwards = ship.body.velocity.x > 0;
         if (shipPositionPastCutoff && shipMovingForwards) {
-			var shipXPositionDiff = ship.position.x - ship.previousPosition.x;
+			var shipXPositionDiff = ship.body.position.x - ship.body.prev.x;
 
             this.__non_player_objects.forEach(function(npo_sprite) {
-                npo_sprite.body.x -= shipXPositionDiff;
+                npo_sprite.body.position.x -= shipXPositionDiff;
 			});
 
-			//ship.x = this.__ship_previous_position.x;
-			//ship.position.x = this.__ship_previous_position.x;
-			//ship.body.x = ship.previousPosition.x;
-
-			//ship.x = this.__parameters.camera_cutoff_x;
-			//ship.position.x = this.__parameters.camara_cutoff_x;
-			//ship.body.x = this.__parameters.camera_cutoff_x;
+			ship.body.position.setTo(ship.body.prev.x,
+									 ship.body.position.y);
         }
 
 
 		this.__checkCollisions();
-
-		blackHole.reset( blackHole.body.x, blackHole.body.y, true );
     },
 
 	__checkCollisions: function()
@@ -156,11 +134,16 @@ Game.prototype =
 		}.bind(this));
 	},
 
+	__shipOutOfBounds : function()
+	{
+		Core.startState( STATE_NAME.GAME_OVER );
+	},
+
 	__onCollision: function(player, npo)
 	{
 		// For now, everything kills the player to we can go straight...
 		//...to the game over state
-		//Core.startState( STATE_NAME.GAME_OVER );
+		Core.startState( STATE_NAME.GAME_OVER );
 	}
 };
 
